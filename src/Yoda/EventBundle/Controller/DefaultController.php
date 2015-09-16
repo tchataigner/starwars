80df1b788f88a4bb338c313ef235dde0a6d55a48
tchataigner <tom.chataigner@yahoo.fr>
2015-09-16 12:27:36 +0200

    Twig

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends Controller
 
         return $this->render(
             'EventBundle:Default:index.html.twig',
-            array('name' => $firstName)
+            array('name' => $firstName, 'count' => $count)
             );
     }
 }
